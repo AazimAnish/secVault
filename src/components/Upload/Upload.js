1754dba85768c927fbc9404ea11d34a66e10a10d
aakash414 <67820270+aakash414@users.noreply.github.com>
2023-03-19 02:40:32 +0530

    upload partially completed

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Upload.css";
+import { create } from "ipfs-core";
 
 function Upload() {
+  const [file, setFile] = useState(null);
+  const [hash, setHash] = useState(null);
+
+  const onFileChange = async (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const uploadFile = async () => {
+    if (file) {
+      const ipfs = await create();
+      const { cid } = await ipfs.add(file);
+      setHash(cid.toString());
+    }
+  };
+
   return (
     <div className="Upload">
       <h2>Upload</h2>
-      <p>This is the Upload Page.</p>
+      <input type="file" onChange={onFileChange} />
+      <button onClick={uploadFile}>Upload</button>
+      {hash && (
+        <p>
+          File uploaded to IPFS! Hash:{" "}
+          <a
+            href={`https://ipfs.io/ipfs/${hash}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {hash}
+          </a>
+        </p>
+      )}
     </div>
   );
 }
